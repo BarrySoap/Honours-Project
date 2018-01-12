4fde6db30a2ed3962a4abc49f3731929bf706720
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-01-12 13:26:56 +0000

    Added some string manipulation and escape characters.

--- Rantaro/Rantaro.py
+++ Rantaro/Rantaro.py
@@ -3,3 +3,12 @@ _name = "Glenn"
 print(6**3)     # Exponentials
 print(9//4)     # Forced Integer Division
 print(9 % 6)    # Modulo
+
+string_one = 'Sentence "One"'
+string_two = "Sentence 'Two'"
+string_three = 'Sentence \'Three\''
+string_four = '''Sentence
+Four
+Is
+Longer'''
+
