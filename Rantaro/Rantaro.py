499b32bdc9b05bddb522cecd95ccfed0f59fd848
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-01-13 12:38:51 +0000

    Testing 2D matrix functions.

--- Rantaro/Rantaro.py
+++ Rantaro/Rantaro.py
@@ -1,42 +1,16 @@
-_name = "Glenn"
+from pprint import pprint as p_print
+from copy import copy, deepcopy
 
-print(6**3)     # Exponentials
-print(9//4)     # Forced Integer Division
-print(9 % 6)    # Modulo
+nums_2d = [
+    [1, 2, 3, 4, 5, 6, 7],
+    [8, 9, 10, 11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20, 21, 22]
+]
 
-string_one = 'Sentence "One"'
-string_two = "Sentence 'Two'"
-string_three = '    Sentence \'Three\'  '
-string_four = '''Sentence
-Four
-Is
-Longer'''
+nums_2d[2][1] = -5
+p_print(nums_2d)
 
-print(string_one.upper())
-print(string_two.title())
-print(string_three.strip().upper())
-
-astr = string_one + string_two
-astr = astr.replace("\"One\"", "Three ")
-astr = astr.replace("Sentence", "Sent")
-print(astr)
-
-first_name = str(input("Enter your first name: "))
-middle_name = str(input("Enter your middle name: "))
-last_name = str(input("Enter your last name: "))
-
-first_name = first_name.capitalize()
-middle_name = middle_name.capitalize()
-last_name = last_name.capitalize()
-
-name_format = "{first} {middle:.1s} {last}"
-print(name_format.format(first=first_name, middle = middle_name, last = last_name))
-
-sentences = [string_one, string_two, string_three, string_four]
-
-alpha = ["a", "b", "c", "d"]
-alpha.append("e")
-alpha = alpha + ["f", "g"]
-d_index = alpha.index("d")
-del alpha[d_index]
-alpha.remove("f")
+letters = ["A", "B", "C", "D", "E", "F"]
+letters_2d = [copy(letters), copy(letters), copy(letters)]
+letters_2d[0][0] = "F"
+p_print(letters_2d)
