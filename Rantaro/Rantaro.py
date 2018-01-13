590331ea2c6ba7d99c20306f34832b134830e8e3
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-01-13 13:15:51 +0000

    Created a volumes module to test the volumes of spheres, cubes and cones.

--- Rantaro/Rantaro.py
+++ Rantaro/Rantaro.py
@@ -1,28 +1,5 @@
-# 3_5 #
-from pprint import pprint as p_print
-from copy import copy, deepcopy
+from Volumes import *
 
-nums_2d = [
-    [1, 2, 3, 4, 5, 6, 7],
-    [8, 9, 10, 11, 12, 13, 14, 15],
-    [16, 17, 18, 19, 20, 21, 22]
-]
-
-nums_2d[2][1] = -5
-p_print(nums_2d)
-
-letters = ["A", "B", "C", "D", "E", "F"]
-letters_2d = [copy(letters), copy(letters), copy(letters)]
-letters_2d[0][0] = "F"
-p_print(letters_2d)
-
-# 3_6 #
-
-a = list(range(0, 10))
-print(a[0:len(a)])
-print(a[::2])
-
-print(a[0:6:2])
-print(a[0:6:3])
-
-print(a[::-1])
+print("Sphere volume with radius 4: " + str(sphere_vol(4)))
+print("Cube volume with length 2, width 3 and height 4: " + str(cube_vol(2, 3, 4)))
+print("Cone volume with radius 3, and height 7: " + str(cone_vol(3, 7)))
