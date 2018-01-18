6fe430d64422df6cc7082cdd4d6019d484b77d85
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-01-18 18:59:20 +0000

    Put everything in methods to avoid cluster.

--- Rantaro/Rantaro.py
+++ Rantaro/Rantaro.py
@@ -8,17 +8,23 @@ input_data = np.array([[5.1, -2.9, 3.3],
 
 # Transforms the data from numerical to boolean using binarization
 # All the values above 2.1 threshold become 1. The remaining values become 0.
-data_binarized = preprocessing.Binarizer(threshold=2.1).transform(input_data)
-print("\nBinarized data:\n", data_binarized)
+def Print_binarized_data():
+    data_binarized = preprocessing.Binarizer(threshold=2.1).transform(input_data)
+    print("\nBinarized data:\n", data_binarized)
 
-# Print mean and standard deviation
-print("\nData set BEFORE:")
-print("Mean =", input_data.mean(axis=0))
-# The .std operator will return the standard deviation of a data set
-print("Std deviation =", input_data.std(axis=0))
+def Print_mean_standard_dev():
+    # Print mean and standard deviation
+    print("\nData set BEFORE:")
+    print("Mean =", input_data.mean(axis=0))
+    # The .std operator will return the standard deviation of a data set
+    print("Std deviation =", input_data.std(axis=0))
 
 # Remove the mean to remove bias from the features in our feature vector
 data_scaled = preprocessing.scale(input_data)
-print("\nAFTER:")
-print("Mean =", data_scaled.mean(axis=0))
-print("Std deviation =", data_scaled.std(axis=0))
+
+def Print_scaled_mean_dev():
+    print("\nAFTER:")
+    print("Mean =", data_scaled.mean(axis=0))
+    print("Std deviation =", data_scaled.std(axis=0))
+
+Print_scaled_mean_dev()
