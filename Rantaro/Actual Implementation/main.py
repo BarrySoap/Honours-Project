37a6af1970b9b38828fde9792cd0b885c62b4919
Glenn Wilkie-Sullivsn <glennpws@hotmail.co.uk>
2018-10-25 16:15:41 +0100

    Added implementation folder for eventual program.

---
+++ Rantaro/Actual Implementation/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 25 16:03:30 2018
+
+@author: Glenn Wilkie-Sullivan
+"""
+
+import neat
+
+genomes = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)]
+
+def run():
+    
+    
+if __name__ == '__main__':
+    run()
