42bdbd5565a8e9dd06b5c3ab752f243213cca828
Glenn Wilkie-Sullivsn <glennpws@hotmail.co.uk>
2018-11-02 20:19:17 +0000

    Initialised population of 50 with files relative to config.

--- Rantaro/Actual Implementation/main.py
+++ Rantaro/Actual Implementation/main.py
@@ -7,9 +7,22 @@ Created on Thu Oct 25 16:03:30 2018
 
 import neat
 
-genomes = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)]
+cooperate = (1, 0)
+defect = (0, 1)
+
+config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation, 'config')
+p = neat.Population(config)
+
+#def evo_alg(networks, config):
+#    for network_id, network in networks:
+#        network.fitness = 4.0
+#        allNets = neat.nn.FeedForwardNetwork.create(network, config)
+        
 
 def run():
+    print(p.population.values())
 
-    if __name__ == '__main__':
-        print("w")
+
+if __name__ == '__main__':
+    run()
