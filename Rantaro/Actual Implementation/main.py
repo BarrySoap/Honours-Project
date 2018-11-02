2929ddc75485ad77573276ae8e510760d2f6b118
Glenn Wilkie-Sullivsn <glennpws@hotmail.co.uk>
2018-11-02 20:47:36 +0000

    Shell comments for ideal program.

--- Rantaro/Actual Implementation/main.py
+++ Rantaro/Actual Implementation/main.py
@@ -14,15 +14,24 @@ config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation, 'config')
 p = neat.Population(config)
 
-#def evo_alg(networks, config):
-#    for network_id, network in networks:
-#        network.fitness = 4.0
-#        allNets = neat.nn.FeedForwardNetwork.create(network, config)
-        
+def evo_alg(networks, config):
+    for network_id_one, playerOne in networks:
+        playerOne.fitness = 4.0
+        allNets = neat.nn.FeedForwardNetwork.create(playerOne, config)
+        for network_id_two, playerTwo in networks:
+            playerTwo.fitness = 4.0
+            allNets.activate(cooperate, defect)
+            # Play one network against another, for each in the network
+            # Ping one neuron as the move, how do they decide?
+            # Update payoffs according to result
+    # Tally payoffs into new fitness
 
 def run():
-    print(p.population.values())
-
+    print("w")
+    # Only gets the fittest network, need to have a roulette wheel
+    # selection for the next generation
+    winner = p.run(evo_alg, 200)
+    # Get fitness of all networks
 
 if __name__ == '__main__':
     run()
