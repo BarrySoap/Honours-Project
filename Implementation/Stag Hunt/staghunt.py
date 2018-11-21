36b0cb641261ac206dd78261f4361ccb9cc76bf8
Glenn Sullivan <glennpws@hotmail.co.uk>
2018-11-21 15:30:00 +0000

    Folder restructure and stag hunt scenario.

--- Implementation/Stag Hunt/staghunt.py
+++ Implementation/Stag Hunt/staghunt.py
@@ -14,69 +14,69 @@ hunt_hare = 1       # Defect
 history = {}
 
 # Container to randomise play
-agent_ids = []
+hunter_ids = []
 # Container for agents
 networks = {}
 
 # Calculate payoff for round, returns number of years in the dilemma scenario
-def Calculate_Payoff(agent_one_move, agent_two_action):
+def Calculate_Payoff(hunter_one_move, hunter_two_action):
 
-    if (agent_one_move == hunt_stag) and (agent_two_action == hunt_hare):
+    if (hunter_one_move == hunt_stag) and (hunter_two_action == hunt_hare):
         return 0
     
-    if (agent_one_move == hunt_hare) and (agent_two_action == hunt_hare):
+    if (hunter_one_move == hunt_hare) and (hunter_two_action == hunt_hare):
         return 1
     
-    if (agent_one_move == hunt_stag) and (agent_two_action == hunt_stag):
+    if (hunter_one_move == hunt_stag) and (hunter_two_action == hunt_stag):
         return 2
 
-    if (agent_one_move == hunt_hare) and (agent_two_action == hunt_stag):
+    if (hunter_one_move == hunt_hare) and (hunter_two_action == hunt_stag):
         return 3
 
 # Play agents against each other
-def evo_alg(agents, config):
+def evo_alg(hunters, config):
 
-    agent_ids.clear()
+    hunter_ids.clear()
     networks.clear()
     
-    for agent_id, agent in agents:
+    for hunter_id, hunter in hunters:
         # Reset agent fitness
-        agent.fitness = 4.0
-        networks[str(agent_id)] = neat.nn.FeedForwardNetwork.create(agent, config)
+        hunter.fitness = 4.0
+        networks[str(hunter_id)] = neat.nn.FeedForwardNetwork.create(hunter, config)
         # Using the history iterator, go back in the history of moves
         # to judge the remaining agents
-        history[str(agent_id)] =  [-1] * hist_iterator
+        history[str(hunter_id)] =  [-1] * hist_iterator
         # Add agent ids
-        agent_ids.append(agent_id)
+        hunter_ids.append(hunter_id)
 
     # Randomise order of play
-    random.shuffle(agent_ids)
+    random.shuffle(hunter_ids)
 
     # Play Round
-    for agent_id, agent in agents:
+    for hunter_id, hunter in hunters:
         
-        agent.fitness = 4.0
+        hunter.fitness = 4.0
 
-        for f in range(len(agents)):
+        for f in range(len(hunters)):
             
             # Initialise opposing agent
-            opponent_id, opponent = agents[f]
+            opponent_id, opponent = hunters[f]
             # Get history of agent's moves
-            agent_history = history[str(agent_id)]      
+            agent_history = history[str(hunter_id)]      
             # Get history of opposing agent's moves
             opponent_history = history[str(opponent_id)]
        
             # Based on the opposing agent's previous actions, determine move to make
-            determineMove = networks[str(agent_id)].activate(opponent_history[-hist_iterator:])
+            determineMove = networks[str(hunter_id)].activate(opponent_history[-hist_iterator:])
             move = np.argmax(determineMove)
-            history[str(agent_id)].append(move)
+            history[str(hunter_id)].append(move)
 
             determineMoveOpponent = networks[str(opponent_id)].activate(agent_history[-hist_iterator:])
             opponent_move = np.argmax(determineMoveOpponent)
             history[str(opponent_id)].append(opponent_move)
 
             # Calculate new fitness
-            agent.fitness += Calculate_Payoff(move, opponent_move) 
+            hunter.fitness += Calculate_Payoff(move, opponent_move) 
             opponent.fitness += Calculate_Payoff(opponent_move, move)
 
 def run():
